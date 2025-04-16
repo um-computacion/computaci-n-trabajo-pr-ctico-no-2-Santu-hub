27ee1e9e8ccea446ee96b6215b95158584263687
santu <santiagobarredo531@gmail.com>
2025-04-15 21:39:05 -0300

    verificacion de texto

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,2 +1,13 @@
 def is_palindrome():
+    cont="y"
+    palabra=input("ingrese la palabra para verificar si es palindrome")   
+    while cont=="y":
+        if any(char.isdigit() for char in palabra):
+            palabra=input("ingrese la palabra para verificar sin numeros")
+        else:
+            print(palabra)
+            cont="n"
     return
+
+if __name__ == "__main__":
+    is_palindrome()
